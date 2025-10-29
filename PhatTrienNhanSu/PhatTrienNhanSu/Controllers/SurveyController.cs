5834020e564dfb6f35396ad4f3996b78f7ee68d8
TranTinhne <Trantinh28012004@gmail.com>
2025-10-29 16:18:51 +0700

    first commit

---
+++ PhatTrienNhanSu/PhatTrienNhanSu/Controllers/SurveyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhatTrienNhanSu.Controllers
+{
+
+    public class SurveyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ PhatTrienNhanSu/PhatTrienNhanSu/Controllers/TrainingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhatTrienNhanSu.Controllers
+{
+    public class TrainingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
